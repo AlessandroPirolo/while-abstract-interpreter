c1430d3e58ee2a349012b6400de5a30a731c1f01
AlessandroPirolo <Alessandro.pirolo@studenti.unipd.it>
2024-02-19 09:03:20 +0100

    Config files and Ast

---
+++ while_abstract_interpreter/lib/ast.ml
@@ -0,0 +1,43 @@
+(* Abstract syntax tree for arithmetic expressions *)
+type aexpr = 
+        | AConst of int
+        | Var of string
+        | ABinOp of aexpr * string * aexpr
+        | IncDec of string * string
+
+(* Abstract syntax tree for boolean expressions *)
+type bexpr = 
+        | BConst of bool  
+        | BUnOp of string * bexpr
+        | BBinOp of bexpr * string * bexpr
+        | BoolRelation of aexpr * string * aexpr
+
+(* Abstract syntax tree for the operations *)
+type statement = 
+        | Assignment of string * aexpr
+        | Skip
+        | Composition of statement * statement
+        | Conditional of bexpr * statement * statement
+        | While of bexpr * statement
+        | Repeat of statement * bexpr
+        
+let rec to_string_a = function
+        | AConst n -> string_of_int n
+        | Var s ->  
+        | ABinOp (a1, s, a2) -> "ABinOp (\n\t" ^ to_string_a a1 ^ ", " ^ s ^ ", \n\t" ^ to_string_a a2 ^ "\n\t)"
+        | IncDec (s, s1) -> "IncDec (" ^ s ^ ", " ^ s1 ^ ")"
+
+let rec to_string_b = function 
+        | BConst true -> "true"
+        | BConst false -> "false"
+        | BUnOp (s, b) -> "BUnOp (" ^ s ^ ", " ^ to_string_b b ^ ")"
+        | BBinOp (b1, s, b2) -> "BBinOp (\n\t" ^ to_string_b b1 ^ ", " ^ s ^ ", \n\t" ^ to_string_b b2 ^ "\n\t)"
+        | BoolRelation (b1, s, b2) -> "BoolRelation (\n\t" ^ to_string_a b1 ^ ", " ^ s ^ ", \n\t" ^ to_string_a b2 ^ "\n\t)"
+
+let rec to_string = function  
+        | Assignment (s, a) -> "Assignment (" ^ s ^ ", " ^ to_string_a a ^ ")"
+        | Skip -> "Skip"
+        | Composition (e1, e2) -> "Composition ( \n\t" ^ to_string e1 ^ ", \n\t" ^ to_string e2 ^ "\n\t)"
+        | Conditional (b, e1, e2) -> "Conditional (" ^ to_string_b b ^  ", \n\t" ^ to_string e1 ^ ", \n\t" ^ to_string e2 ^ "\n\t)"
+        | While (b, s) -> "While (" ^ to_string_b b ^ ", \n\t" ^ to_string s ^ "\n\t)"
+        | Repeat (s, b) ->  "Repeat (\n\t" ^ to_string s ^ ", \n\t" ^ to_string_b b ^ "\n\t)"
