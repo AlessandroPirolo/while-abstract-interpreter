c3ca2ae9ac9e38d2345ee9e0123dfc681a5f3ad5
AlessandroPirolo <Alessandro.pirolo@studenti.unipd.it>
2024-04-21 21:54:20 +0200

    Update Eval.fs
    While improved and now eval return a state and list of states

--- Evaluator/Eval.fs
+++ Evaluator/Eval.fs
@@ -4,52 +4,54 @@ open Ast
 open EvalBexpr
 open AbstractState
 open EvalAexpr
-open Interval
 
 exception Error of string
 
-let rec eval (stmt : Statement) (state : State) (states : State list) : State =
+let rec eval (stmt : Statement) (state : State) (states : State list) : State * State list =
   match stmt with
-  | Skip -> state
+  | Skip -> (state, states)
   | Composition (expr1, expr2) ->
-      let state1 = eval expr1 state states
-      eval expr2 state1 (states @ [ state1 ])
+      let state1, states1 = eval expr1 state states
+      let state2, states2 = eval expr2 state1 states1
+      (state2, states2)
   | Assignment (str, aexpr) ->
       let res = eval_aexpr aexpr state 
       let new_state = add str res state
       
-      new_state
-
+      (new_state, (states @ [new_state]))
   | Conditional (cond, expr1, expr2) ->
       let true_cond = eval_bexpr cond state 
       let false_cond = eval_bexpr (BUnOp ("!", cond)) state 
-      let t = if isEmpty true_cond then Map.empty else eval expr1 true_cond [] 
-      let f = if isEmpty false_cond then Map.empty else eval expr2 false_cond [] 
+      let t, true_branch_state = if isEmpty true_cond then (Map.empty, []) else eval expr1 true_cond [] 
+      let f, false_branch_state = if isEmpty false_cond then (Map.empty, []) else eval expr2 false_cond [] 
       
-      union t f
+      (union t f, states @ true_branch_state @ false_branch_state)
       
   | IncDec (e, op) -> 
       let value = find e state 
-      match op with
-      | "++" -> add e (value.AbstractInc) state
-      | "--" -> add e (value.AbstractDec) state
-      | _ -> state
+      let new_state = 
+        match op with
+        | "++" -> add e (value.AbstractInc) state
+        | "--" -> add e (value.AbstractDec) state
+        | _ -> state
+
+      (new_state, states @ [new_state])
      
   | While (bexpr, expr) -> 
       (* B[-b](lfp(fun x -> s V (D[S]oB[b])x )) *)
       let b = eval_bexpr bexpr state
-      let not_b = eval_bexpr (BUnOp("!", bexpr)) state 
+       
       let mutable fixpoint = false 
-      let mutable prev_state = eval expr b []  
+      let mutable prev_state, _ = eval expr b []  
       let mutable succ_state = prev_state 
       while not fixpoint do 
         let nb = eval_bexpr bexpr prev_state 
-        succ_state <- eval expr nb []
+        succ_state <- fst(eval expr nb [])
         let un = union prev_state succ_state
         let wide = widening prev_state un
-        if wide = succ_state then
+        if wide = un then
           fixpoint <- true
         else
-          prev_state <- succ_state
-
-      eval expr not_b []
+          prev_state <- wide
+      printfn "succ state %s" (to_string succ_state)
+      (eval_bexpr (BUnOp("!", bexpr)) succ_state, [])
