84c626427f08217ab36adff05e2c4a8a67ae69d9
AlessandroPirolo <Alessandro.pirolo@studenti.unipd.it>
2024-05-06 21:28:47 +0200

    Evaluator improved
    Fixed while evaluation bug, now eval returns the result and some invariants (if there are any). In Program, create a function trasforming a State Option list into string

--- Evaluator/Eval.fs
+++ Evaluator/Eval.fs
@@ -7,25 +7,25 @@ open EvalAexpr
 
 exception Error of string
 
-let rec eval (stmt : Statement) (state : State) (states : State list) : State * State list =
+let rec eval (stmt : Statement) (state : State) : State * State Option list =
   match stmt with
-  | Skip -> (state, states)
+  | Skip -> (state, [])
   | Composition (expr1, expr2) ->
-      let state1, states1 = eval expr1 state states
-      let state2, states2 = eval expr2 state1 states1
-      (state2, states2)
+      let state1, inv1 = eval expr1 state
+      let state2, inv2 = eval expr2 state1
+      (state2, inv1 @ inv2)
   | Assignment (str, aexpr) ->
       let res = eval_aexpr aexpr state 
       let new_state = add str res state
       
-      (new_state, (states @ [new_state]))
+      (new_state, [])
   | Conditional (cond, expr1, expr2) ->
       let true_cond = eval_bexpr cond state 
       let false_cond = eval_bexpr (BUnOp ("!", cond)) state 
-      let t, true_branch_state = if isEmpty true_cond then (Map.empty, []) else eval expr1 true_cond [] 
-      let f, false_branch_state = if isEmpty false_cond then (Map.empty, []) else eval expr2 false_cond [] 
+      let t, invf = if isEmpty true_cond then (Map.empty, []) else eval expr1 true_cond 
+      let f, invt = if isEmpty false_cond then (Map.empty, []) else eval expr2 false_cond 
       
-      (union t f, states @ true_branch_state @ false_branch_state)
+      (union t f, invt @ invf)
       
   | IncDec (e, op) -> 
       let value = find e state 
@@ -35,40 +35,29 @@ let rec eval (stmt : Statement) (state : State) (states : State list) : State * 
         | "--" -> add e (value.AbstractDec) state
         | _ -> state
 
-      (new_state, states @ [new_state])
+      (new_state, [])
      
   | While (bexpr, expr) -> 
       (* B[-b](lfp(fun x -> s V (D[S]oB[b])x )) *)
-      let b = eval_bexpr bexpr state
-      //printfn "b is %s" (to_string b)
       let mutable fixpoint = false 
-      let mutable prev_state = b  
-      let mutable succ_state, c = eval expr b []
+      let mutable curr = state 
+      let mutable succ = Map.empty
+      let mutable inv : State Option = Some Map.empty
 
       while not fixpoint do 
-        let un = union prev_state succ_state
-        //printfn "prev_state is %s" (to_string prev_state)
+        let b = eval_bexpr bexpr curr
+        inv <- Some b
+        let eval_expr, _ = eval expr b
+        succ <- eval_expr
+        let un = union curr succ
+        let wide = widening1 curr un
 
-        //printfn "un is %s" (to_string un)
-
-        let wide = widening1 prev_state un
-        //printfn "wide is %s" (to_string wide)
-        let nb = eval_bexpr bexpr un
-        //printfn "nb is %s" (to_string nb)
-
-        succ_state <- fst(eval expr nb [])
-        //printfn "succ is %s" (to_string succ_state)
-
-        c <- snd(eval expr nb [])
-        
         if wide = un then
           fixpoint <- true
         else
-          prev_state <- wide
+          curr <- wide
           
-      //printfn "prev is %s" (to_string prev_state)
-      let post_cond = eval_bexpr (BUnOp("!", bexpr)) prev_state
-      //printfn "post is %s" (to_string post_cond)
+      let exit_cond = eval_bexpr (BUnOp("!", bexpr)) curr
+      let nar = narrowing exit_cond succ
 
-      let nar = narrowing post_cond succ_state
-      (nar, [])
+      (nar, [inv])
--- Program.fs
+++ Program.fs
@@ -5,19 +5,29 @@ open FSharp.Text.Lexing
 open Eval
 open AbstractState
 
+let rec invariants_to_string (invs : State Option list) : string =
+    match invs with 
+    | [] -> "\n"
+    | h::t -> 
+        let res = 
+            match h with
+            | Some inv -> "invariant 1: " + to_string inv 
+            | None -> ""
+        res + invariants_to_string t
+
 let parse (ds : string) : Statement  = 
     let lexbuf = LexBuffer<char>.FromString ds
     Parser.prog Lexer.tokenize lexbuf
     
 [<EntryPoint>]
 let main argv =
-    let program =  "x := 1; \n while x < 9 do x++" 
-    
+    let program =  "x := 10; \n while x > 0 do x--  " 
+
     let stmt = parse program
     printfn "%s" stmt.ToString
     let initial_state = init_state stmt
     printfn "initial state %s" (to_string initial_state)
-    let result, states = eval stmt initial_state [initial_state]
-    printfn "iterations: %A" (List.map (fun s -> to_string s + "\n") states)
+    let result, inv = eval stmt initial_state
     printfn "result %s" (to_string result)
+    printfn "%s" (invariants_to_string inv)
     0
