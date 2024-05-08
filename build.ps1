f96e6a0c42532874fc13cf6e5b92cb329e7c81e7
AlessandroPirolo <Alessandro.pirolo@studenti.unipd.it>
2024-05-08 16:09:59 +0200

    Modified building script

--- build.ps1
+++ build.ps1
@@ -1,9 +1,3 @@
-param (
-    [string]$argument1,
-    [string]$argument2,
-    [string]$argument3
-)
-
 # Specify the path to your .fsproj file
 $projectPath = "WhileAbstractInterpreter.fsproj"
 
@@ -13,7 +7,6 @@ Set-Location (Split-Path $projectPath)
 # Build the project using dotnet build
 dotnet build $projectPath
 
-# Run the generated executable
-$output = & ".\bin\Debug\net7.0\WhileAbstractInterpreter.exe" $argument1 $argument2 $argument3
-
-Write-Output $output
+# Move executable 
+rm .\WhileAbstractInterpreter.exe
+ mv .\bin\Debug\net7.0\WhileAbstractInterpreter.exe ..\while-abstract-interpreter\
